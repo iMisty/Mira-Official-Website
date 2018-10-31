6cd5dd4c5649d6d94eb2db3ce84bbbf6c28ae5cd
Miya <qianwumeng_2cyndi@qq.com>
2018-11-01 00:04:02 +0800

    Update src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <wrap/>
+  </div>
+</template>
+
+<script>
+import wrap from './components/wrap'
+
+export default {
+  name: 'app',
+  components: {
+    wrap
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0,0,0,.28);
+  height: 100vh;
+}
+</style>
