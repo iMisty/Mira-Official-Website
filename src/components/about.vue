9a3848be9d444f52ce7af158445d3dca55867228
Miya <qianwumeng_2cyndi@qq.com>
2018-11-01 00:04:03 +0800

    Update src/components/about.vue

---
+++ src/components/about.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <h1>2</h1>
+</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style scoped>
+</style>
