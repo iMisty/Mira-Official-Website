dedf6e1f970b7729560dff1419dd5ff903bb0107
Miya <qianwumeng_2cyndi@qq.com>
2018-11-01 00:04:08 +0800

    Update src/components/logo.vue

---
+++ src/components/logo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <img :src="imgUrl" alt="avatar">
+    </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data () {
+    return {
+        imgUrl: '/static/avatar.png'
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style lang="less" scoped>
+div{
+    margin-bottom: 1.2rem;
+    border-radius: 100%;
+    img{
+        border: 6px solid #fff;
+        border-radius: 100%;
+        max-width: 148px;
+        max-height: 148px;
+    }
+}
+</style>
