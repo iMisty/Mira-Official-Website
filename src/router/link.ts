2f37041420dca7bc0f358ee3f55af3ef62e2ba90
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-12-06 18:05:08 +0800

    Update link.ts

--- src/router/link.ts
+++ src/router/link.ts
@@ -2,9 +2,9 @@
  * @Author: Miya
  * @Date: 2020-07-15 00:50:17
  * @LastEditors: Miya
- * @LastEditTime: 2020-07-30 04:12:08
+ * @LastEditTime: 2020-12-10 14:51:19
  * @Description: 导航链接路由
- * @FilePath: \Single-Search-Server\src\router\link.ts
+ * @FilePath: \Single-Search-APIc:\Users\Platinum Prism\Documents\GitHub\Single-Search-Backend\src\router\link.ts
  */
 import * as Router from 'koa-router';
 const router = new Router();
@@ -16,11 +16,9 @@ router.prefix('/link');
 // 查询链接
 router.get('/', Link.searchLink);
 // 增加链接
-router.post('/add', Link.addNewLink);
-// 修改链接
-// router.put('/', Link.updateLink);
+router.post('/', Link.addNewLink);
+//修改链接
+router.put('/', Link.changeLink);
 // 删除链接
 // router.delete('/', Link.removeLink);
-// 条件筛选 => 根据类目
-router.get('/search', Link.screenLink);
 export default router;
