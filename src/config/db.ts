47607f3837d20b09977a262ba2b28b7028936813
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-12-07 18:05:20 +0800

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,10 @@
  * @Author: Miya
  * @Date: 2020-07-14 17:26:48
  * @LastEditors: Miya
- * @LastEditTime: 2020-07-14 17:27:49
+ * @LastEditTime: 2020-12-10 11:08:57
  * @Description: 定义MongoDB位置
- * @FilePath: \server2\src\config\db.ts
+ * @FilePath: \Single-Search-APIc:\Users\Platinum Prism\Documents\GitHub\Single-Search-Backend\src\config\db.ts
  */
-const dbs = 'mongodb://localhost:27017/dbs';
+const dbs = 'mongodb://localhost:27017/mdb_search';
 
 module.exports = { dbs };
