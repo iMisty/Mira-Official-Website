475c334dbae11b81d58769fa91e0b0ac6c24c78e
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2020-12-10 18:06:22 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,35 @@
 /*
  * @Author: Miya
  * @Date: 2020-07-26 03:57:56
- * @LastEditTime: 2020-10-15 02:15:44
+ * @LastEditTime: 2020-12-10 10:55:02
  * @LastEditors: Miya
  * @Description: 路由首页
- * @FilePath: /Single-Search-Backend/src/router/index.ts
+ * @FilePath: \Single-Search-APIc:\Users\Platinum Prism\Documents\GitHub\Single-Search-Backend\src\router\index.ts
  */
-import * as Router from 'koa-router';
-import * as combineRouters from 'koa-combine-routers';
+import * as Router from "koa-router";
+import * as combineRouters from "koa-combine-routers";
 // 测试用
-import testRouter from './test';
+import testRouter from "./test";
 // 链接数据
-import linkRouter from './link';
+import linkRouter from "./link";
 // 管理员及登录相关
-import adminRouter from './admin';
+import adminRouter from "./admin";
+const Admin = require("../controller/Admin");
 
 const router = new Router();
 
-const index = router.get('/', async (ctx) => {
-	ctx.body = 'Hello TypeScript';
+const index = router.get("/", async (ctx: { body: string }) => {
+  ctx.body = "Hello TypeScript";
 });
 
-const routers = combineRouters(index,linkRouter, adminRouter, testRouter);
+const login = router.post("/login", Admin.login);
+
+const routers = combineRouters(
+  index,
+  login,
+  linkRouter,
+  adminRouter,
+  testRouter
+);
 
 export default routers;
