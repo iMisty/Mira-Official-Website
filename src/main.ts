668736f48bd257b5817187a8fb621196f18df1ff
Kagura Koeri <16733568+iMisty@users.noreply.github.com>
2022-04-24 11:51:52 +0800

    Remove: Remove

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-import store from './store/index';
-import router from './router/index';
-import App from './App';
-
-createApp(App).use(router).use(store).mount('#app');
