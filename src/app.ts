57c4e0b37b1524767cbe5cb53c54cdb5b3a10374
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2022-05-04 02:55:14 +0800

    Delete: Koa-view

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,11 @@
  * @Version: 1.0
  * @Author: Mirage
  * @Date: 2022-04-24 12:00:55
- * @LastEditors: Mirage
- * @LastEditTime: 2022-04-24 16:42:19
+ * @LastEditors: Miya
+ * @LastEditTime: 2022-05-03 21:46:13
  */
 import Koa from 'koa';
 const app = new Koa();
-import views from 'koa-views';
 import json from 'koa-json';
 import bodyparser from 'koa-bodyparser';
 import logger from 'koa-logger';
@@ -22,14 +21,9 @@ app.use(
 app.use(json());
 app.use(logger());
 app.use(require('koa-static')(__dirname + '/public'));
-app.use(
-  views(__dirname + '/views', {
-    extension: 'ejs',
-  })
-);
 app.use(router());
 app.listen(7752);
-console.log('Dirname:', __dirname);
+console.log('APP Listening on Port: 7752');
 
 // logger
 app.use(async (ctx, next) => {
