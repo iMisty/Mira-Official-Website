5150d6aae852b374de59c654d23514e9e8f7dce9
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2023-11-01 00:44:57 +0800

    WIP: Homepage

---
+++ homepage/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * @Author: Miya
+ * @Date: 2023-11-01 00:13:26
+ * @LastEditTime: 2023-11-01 00:13:26
+ * @LastEditors: Miya
+ * @Description: Script
+ * @FilePath: \homepage\js\script.js
+ */
